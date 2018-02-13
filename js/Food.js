16254b699f4144d17321c87652c7793228e460e8
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-02-13 17:45:29 -0500

    Worked on Food eat() function.

--- js/Food.js
+++ js/Food.js
@@ -1,12 +1,18 @@
 /**
  * @author Joshua Ciffer
- * @version 02/12/2018
+ * @version 02/13/2018
  */
 function Food() {
 	this.pos = createVector(floor(random(width)), floor(random(height)));
 	
 	this.respawnFood = function() {
 		this.pos = createVector(floor(random(width)), floor(random(height)));
+	}
+	
+	this.eat = function(Snake) {
+		if(dist(this.pos.x, this.pos.y, Snake.pos.x, Snake.pos.y) < 1) {
+			Food.respawnFoood();
+		}
 	}
 	
 }
